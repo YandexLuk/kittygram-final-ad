b6573946c533e7ea6efd216858c90971f87a4699
YandexLuk <dragonluk78@yandex.ru>
2026-04-24 02:06:02 +0400

    Final

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+echo "Waiting for database..."
+while ! python -c "
+import os, psycopg2
+conn = psycopg2.connect(
+    dbname=os.getenv('POSTGRES_DB'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT')
+)
+conn.close()
+" 2>/dev/null; do
+  sleep 1
+done
+echo "Database is ready!"
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /backend_static/static/
